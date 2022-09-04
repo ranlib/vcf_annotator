e48d9eba222bc3cda82db74b4a56e6f6e12ec5cd
Dieter Best <dieterbest@gmail.com>
2022-09-04 12:32:44 -0700

    some formatting

--- vcf_annotator_post.py
+++ vcf_annotator_post.py
@@ -4,7 +4,7 @@ Purpose:
 get annotation for vcf file from EnsEmbl via rest calls
 
 Usage:
-usage: vcf_annotator_post.py [-h] -i VCF_INPUT -o TSV_OUTPUT -j JSON_OUTPUT [-r SERVER] [-v] [-q] [-m]
+vcf_annotator_post.py [-h] -i VCF_INPUT -o TSV_OUTPUT -j JSON_OUTPUT [-r SERVER] [-v] [-q] [-m]
 
 get annotation for vcf file
 
@@ -22,12 +22,26 @@ optional arguments:
   -q, --query           turn on rest calls to server for gene information
   -m, --maf             turn on rest calls to server for MAF information
 
-Comments:
-1) -q parameter: turn on at first run in order to produce the json file with the annotation information
-   for a vcf file with about 11,000 rows this takes about 20 min with bulk rest call, 200 entries at a time
-2) -m parameter: turn on if Minor Allele Frequency should be added to output tsv file,
-   rest call for one entry at a time, for vcf file with about 11,000 rows this takes about 2 hours
-   one-at-a-time calls because no all variants have annotation information
+Documentation:
+1) -q parameter: turn on at first run in order to produce the json
+   file with the annotation information for a vcf file with about
+   11,000 rows this takes about 20 min with bulk rest call, 200
+   entries at a time
+2) -m parameter: turn on if Minor Allele Frequency should be added to
+   output tsv file, rest call for one entry at a time, for vcf file
+   with about 11,000 rows this takes about 2 hours one-at-a-time calls
+   because no all variants have annotation information
+3) data structure that hold the information and information is added
+   to variants = dictionary index on hgvs terms, each entry =
+   dictionary of annotation information
+4) procedure has 6 steps:
+   a) read in vcf file
+   b) add information from vcf file to variants data structure
+   c) get via REST calls to EnsEmbl annotation information in json
+   format for hgvs terms in variants data structure
+   d) parse json and add information to variants data strucutre
+   e) get rsid information from variants data structure and via REST call to EnsEmbl get MAF
+   f) write variants data structure (dictionary of dictionaries) to tsv file
 
 Notes:
 1. Depth of sequence coverage at the site of variation.
@@ -123,11 +137,10 @@ def vcf_annotator_post(params):
     get annotation for variant via REST from EnsEmbl
 
     Input:
-    vcf_input: str: input vcf file
-    verbose: bool: turn on verbose debugging output
+    params: command line parameters
 
     Output:
-    tsv_output: str: output tsv file
+    tsv_output: str: output tsv file given by command line
     """
 
     # setup vcf reader
